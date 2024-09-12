354822ecdfc6a7496da9990dc2d255f2c154fadc
Salman <salman_cntrt@deskfactors.com>
2024-09-12 18:35:03 -0400

    removeDuplicates

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int l = 1;
+        int r = 1;
+        
+                while (r < nums.length) {
+                    if (nums[r] != nums[r-1]) {
+                        nums[l] = nums[r];
+                        l++;
+                    }
+                    r++;
+                }
+            return l;
+        
+    }
+}
