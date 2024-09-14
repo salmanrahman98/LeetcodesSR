a55146abbc4e8ef8514911e66abd519c998eff31
salmanrahman98 <salmanrahman1998@gmail.com>
2024-09-14 13:51:29 -0400

    remove element push

--- Arrays/RemoveElement.java
+++ Arrays/RemoveElement.java
@@ -4,7 +4,7 @@ package Arrays;
 public class RemoveElement {
         public int removeElement(int[] nums, int val) {
             
-            int j = 1;
+            int j = 0;
             for(int i =0;i<nums.length;i++){
                 if(nums[i]!=val){
                     nums[j] = nums[i];
