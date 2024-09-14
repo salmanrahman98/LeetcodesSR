9057e7bb203ff39a13b2686266ee80bb52495e77
Salman <salman_cntrt@deskfactors.com>
2024-09-14 13:45:00 -0400

    test

--- Arrays/RemoveElement.java
+++ Arrays/RemoveElement.java
@@ -4,7 +4,7 @@ package Arrays;
 public class RemoveElement {
         public int removeElement(int[] nums, int val) {
             
-            int j = 0;
+            int j = 1;
             for(int i =0;i<nums.length;i++){
                 if(nums[i]!=val){
                     nums[j] = nums[i];
