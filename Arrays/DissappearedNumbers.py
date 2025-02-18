db46e1ba61e9802a49cd4402a2e96075f82cbc4a
salmanrahman98 <salmanrahman1998@gmail.com>
2025-02-17 20:05:08 -0500

    Dissappearing Commit

---
+++ Arrays/DissappearedNumbers.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    zero_list = [0] * (len(nums))
+
+    for i in range(0, len(nums)):
+        zero_list[nums[i]-1] = 1
+
+    final_list = []
+    for i in range(0, len(zero_list)):
+        if zero_list[i] == 0:
+            final_list.append(i+1)
+    return final_list
+
+
+nums = [4, 3, 2, 7, 8, 2, 3, 1]
+findDisappearedNumbers(0, nums=nums)
+# print(findDisappearedNumbers(0, nums))
