1e89ee0b9eb39adee40c9ccf754281abc81d857e
salmanrahman98 <salmanrahman1998@gmail.com>
2025-02-23 18:08:37 -0500

    code cleanup

--- Arrays/spiralmatrix.py
+++ Arrays/spiralmatrix.py
@@ -1,37 +1,27 @@
 # https://leetcode.com/problems/spiral-matrix/description/
-
 def go_left_to_right(start_from, go_till, step_over_by, in_row, matrix, arr):
     for i in range(start_from, go_till, step_over_by):
-        print(matrix[in_row][i])
         arr.append(matrix[in_row][i])
 
 
 def go_top_to_down(start_from, go_till, step_over_by, in_column, matrix, arr):
     for i in range(start_from, go_till, step_over_by):
-        print(matrix[i][in_column])
         arr.append(matrix[i][in_column])
         matrix[i].pop(in_column)
 
 
 def right_to_left(start_from, go_till, step_over_by, in_row, matrix, arr):
     for i in range(start_from, go_till, step_over_by):
-        print(matrix[in_row][i])
         arr.append(matrix[in_row][i])
 
 
 def down_to_top(start_from, go_till, step_over_by, in_column, matrix, arr):
     for i in range(start_from, go_till, step_over_by):
-        print(matrix[i][in_column])
         arr.append(matrix[i][in_column])
         matrix[i].pop(in_column)
 
 
 def spiralOrder(matrix):
-
-    rows_length = len(matrix)
-    cols_length = len(matrix[0])
-
-    # for x in range(rows_length * cols_length):
     arr = []
     while True:
         go_left_to_right(start_from=0, go_till=len(matrix[0]),
@@ -43,7 +33,6 @@ def spiralOrder(matrix):
 
         go_top_to_down(start_from=0, go_till=len(matrix),
                        step_over_by=1, in_column=len(matrix[0])-1, matrix=matrix, arr=arr)
-        print(matrix)
         if (len(matrix) == 0 or len(matrix[0]) == 0):
             break
 
@@ -52,36 +41,13 @@ def spiralOrder(matrix):
 
         matrix.pop(len(matrix)-1)
 
-        print(matrix)
         if (len(matrix) == 0 or len(matrix[0]) == 0):
             break
-        print(arr)
         down_to_top(len(matrix)-1, go_till=-1, step_over_by=-
                     1, in_column=0, matrix=matrix, arr=arr)
 
         if (len(matrix) == 0 or len(matrix[0]) == 0):
             break
-
-    # print(len(matrix))
-    # arr = []
-    # i = 0
-    # while i < rows_length * cols_length:
-    #     for
-    #     arr.append()
-    # printing_rows = True
-    # overall_count = 0
-
-    # while overall_count < rows_length * cols_length:
-
-    # start_from = rows_length - 1
-    # go_till = -1
-    # step_over_by = -1
-
-    # for i in range(start_from, go_till, step_over_by):
-    #     print(matrix[i][0])
-    # for j in range(0, cols_length):
-
-    #     print(matrix[j][i])
 
     return arr
 
